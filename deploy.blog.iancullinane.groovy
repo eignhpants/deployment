aa3dfc9f3824d8b38128ff6a739c5f51022a77ea
eignhpants <icullinane@turbine.com>
2016-07-13 14:25:13 -0400

    remove -d

--- deploy.blog.iancullinane.groovy
+++ deploy.blog.iancullinane.groovy
@@ -14,7 +14,7 @@ node(NODE_LABEL){
 
 
     stage "Deploy Blog"
-    sh "docker run -d --rm \
+    sh "docker run --rm \
         --name iancullinane.com \
         -p 2368:2368 -v /var/lib/ghost/iancullinane:/var/lib/ghost \
         --restart=always \
